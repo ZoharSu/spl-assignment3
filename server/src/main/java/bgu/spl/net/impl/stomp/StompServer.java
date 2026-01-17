6a1883e7378db46de3a472b96934b193a70bc20e
zoxarc <zoxarc@gmail.com>
2026-01-17 10:30:10 +0200

    Finish StompServer, maybe needs a new encdec

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,35 +1,32 @@
 package bgu.spl.net.impl.stomp;
 
-import bgu.spl.net.impl.rci.ObjectEncoderDecoder;
+import bgu.spl.net.impl.echo.LineMessageEncoderDecoder;
 import bgu.spl.net.srv.Server;
 
 public class StompServer {
 
     public static void main(String[] args) {
-        // TODO: implement this
         if (args.length < 2) {
-            System.out.println("Arguments: <port> {tcp | reactor} [thread count]");
+            System.out.println("Arguments: <port> {tcp | reactor}");
             return;
         }
 
         int port = Integer.parseInt(args[0]);
-        int nthreads = args.length < 3 ? 10 : Integer.parseInt(args[3]);
 
-        switch (args[1]) {
-            case "tpc":     tpcMain(port); break;
-            case "reactor": reactorMain(port, nthreads); break;
-
-        }
+        if (args[1] == "tpc")
+            Server.threadPerClient(
+                    port, //port
+                    StompProtocol::new, //protocol factory
+                    LineMessageEncoderDecoder::new //message encoder decoder factory
+            ).serve();
+        else if (args[1] == "reactor")
+            Server.reactor(
+                    Runtime.getRuntime().availableProcessors(),
+                    port, //port
+                    StompProtocol::new, //protocol factory
+                    LineMessageEncoderDecoder::new //message encoder decoder factory
+            ).serve();
+        else
+            System.out.println("Arguments: <port> {tcp | reactor}");
     }
-
-    private static void tpcMain(int port) {
-        Server.threadPerClient(port, StompProtocol::new , ObjectEncoderDecoder::new);
-    }
-
-    private static void reactorMain(int port, int nthreads) {
-        // TODO: reactor
-        // Server.reactor(nthreads, port, protocolFactory, ObjectEncoderDecoder::new);
-        throw new IllegalArgumentException("TODO");
-    }
-
 }
